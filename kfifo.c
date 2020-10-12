26936c90256d8cbef3d10fa5ef5724727d8ebff1
cary.zhou <kaichuangluo@gmail.com>
2020-10-13 00:11:28 +0800

    Update kfifo.c

--- kfifo.c
+++ kfifo.c
@@ -1,15 +1,11 @@
-//当fifo->in == fifo->out时,buffer为空
-//当fifo->size == (fifo->in - fifo->out)时,buffer已满
-
 //入队
 unsigned int kfifo_put(struct kfifo *fifo, unsigned char *buffer, unsigned int len)
 {
     unsigned int l;
-	//fifo->size - fifo->in + fifo->out:空闲长度
+    //fifo->size - fifo->in + fifo->out:空闲长度
     len = min(len, fifo->size - fifo->in + fifo->out);//计算可写长度
-	//fifo->size - (fifo->in & (fifo->size - 1)):写指针到数组末尾长度
+    //fifo->size - (fifo->in & (fifo->size - 1)):写指针到数组末尾长度
     l = min(len, fifo->size - (fifo->in & (fifo->size - 1)));
-	//fifo->in & (fifo->size - 1):写指针
     memcpy(fifo->buffer + (fifo->in & (fifo->size - 1)), buffer, l);
     memcpy(fifo->buffer, buffer + l, len - l);
     fifo->in += len;
@@ -20,11 +16,10 @@ unsigned int kfifo_put(struct kfifo *fifo, unsigned char *buffer, unsigned int l
 unsigned int kfifo_get(struct kfifo *fifo, unsigned char *buffer, unsigned int len)
 {
     unsigned int l;
-	//fifo->in - fifo->out:数据长度
+    //fifo->in - fifo->out:数据长度
     len = min(len, fifo->in - fifo->out);
-	//fifo->size - (fifo->out & (fifo->size - 1)):读指针到数组末尾长度
+    //fifo->size - (fifo->out & (fifo->size - 1)):读指针到数组末尾长度
     l = min(len, fifo->size - (fifo->out & (fifo->size - 1)));
-	//fifo->out & (fifo->size - 1):读指针
     memcpy(buffer, fifo->buffer + (fifo->out & (fifo->size - 1)), l);
     memcpy(buffer + l, fifo->buffer, len - l);
     smp_mb();
