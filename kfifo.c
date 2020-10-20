c5b4c78b7c4dd6ed525dc50b0ab30a3340584e82
cary.zhou <kaichuangluo@gmail.com>
2020-10-20 21:22:15 +0800

    Update kfifo.c

--- kfifo.c
+++ kfifo.c
@@ -4,7 +4,7 @@ unsigned int kfifo_put(struct kfifo *fifo, unsigned char *buffer, unsigned int l
     unsigned int l;
     //fifo->size - fifo->in + fifo->out:空闲长度
     len = min(len, fifo->size - fifo->in + fifo->out);//计算可写长度
-    //fifo->size - (fifo->in & (fifo->size - 1)):写指针到数组末尾长度
+    //fifo->size - (fifo->in & (fifo->size - 1)):写指针到数组末尾的长度,即一次最大可写长度
     l = min(len, fifo->size - (fifo->in & (fifo->size - 1)));
     memcpy(fifo->buffer + (fifo->in & (fifo->size - 1)), buffer, l);
     memcpy(fifo->buffer, buffer + l, len - l);
@@ -18,7 +18,7 @@ unsigned int kfifo_get(struct kfifo *fifo, unsigned char *buffer, unsigned int l
     unsigned int l;
     //fifo->in - fifo->out:数据长度
     len = min(len, fifo->in - fifo->out);
-    //fifo->size - (fifo->out & (fifo->size - 1)):读指针到数组末尾长度
+    //fifo->size - (fifo->out & (fifo->size - 1)):读指针到数组末尾长度,即一次最大可读长度
     l = min(len, fifo->size - (fifo->out & (fifo->size - 1)));
     memcpy(buffer, fifo->buffer + (fifo->out & (fifo->size - 1)), l);
     memcpy(buffer + l, fifo->buffer, len - l);
